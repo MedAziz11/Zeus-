6cc8a90fe606844464b687d121a611f7147a783f
MedAziz11 <chagourmedaziz@gmail.com>
2021-07-24 19:24:43 +0100

    init commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 970,
+    height: 550,
+    nodeIntegration: true,
+    contextIsolation: true,
+    devTools: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('src/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
